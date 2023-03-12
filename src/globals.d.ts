e8e1f794289c6ce93f6525d41671063939e9ba8f
Mike DiDomizio <mikedidomizio@gmail.com>
2023-03-12 15:12:24 -0400

    Remove globals file, unneeded

--- src/globals.d.ts
+++
@@ -1,8 +0,0 @@
-import type { DangerDSLType } from 'danger'
-
-// https://github.com/danger/danger-js/issues/1152
-declare global {
-  // import * as danger1 from 'danger'
-  //
-  // const danger: DangerDSLType = danger1.danger
-}
