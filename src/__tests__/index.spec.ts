889a5cbfed01c8fddbef41f883493a2896eb6b15
Mike DiDomizio <mikedidomizio@gmail.com>
2023-03-12 14:12:41 -0400

    Move dev-deps to deps, add tests for all danger functions

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,19 +1,67 @@
-import { dangerTesting, dm } from '../'
+import { dangerTesting, dm, fail, markdown, message, warn } from '../'
 
-jest.mock('danger', () => jest.fn())
+describe('dangerTesting', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
 
-describe('dangerTest', () => {
-  it('should validate with `message` function', () => {
+  it('should call `fail` function if criteria not met', () => {
     const fn = () => {
-      if (!dm.github?.pr.title.match(/^\[\w+-\d+]/)) {
-        dm.message(`PR title does not match expectation`)
+      if (!dm.github.pr.title.match(/^\[\w+-\d+]/)) {
+        fail(`PR title does not match expectation`)
       }
     }
 
     dangerTesting(fn, { github: { pr: { title: 'hello world' } } })
-    expect(dm.message).toHaveBeenCalledWith(
-      'PR title does not match expectation',
-    )
-    expect(dm.message).toHaveBeenCalledTimes(1)
+    expect(fail).toHaveBeenCalledWith('PR title does not match expectation')
+    expect(fail).toHaveBeenCalledTimes(1)
+    expect(markdown).not.toHaveBeenCalled()
+    expect(message).not.toHaveBeenCalled()
+    expect(warn).not.toHaveBeenCalled()
+  })
+
+  it('should call `markdown` function if criteria not met', () => {
+    const fn = () => {
+      if (!dm.github.pr.title.match(/^\[\w+-\d+]/)) {
+        markdown(`PR title does not match expectation`)
+      }
+    }
+
+    dangerTesting(fn, { github: { pr: { title: 'hello world' } } })
+    expect(markdown).toHaveBeenCalledWith('PR title does not match expectation')
+    expect(markdown).toHaveBeenCalledTimes(1)
+    expect(fail).not.toHaveBeenCalled()
+    expect(message).not.toHaveBeenCalled()
+    expect(warn).not.toHaveBeenCalled()
+  })
+
+  it('should call `message` function if criteria not met', () => {
+    const fn = () => {
+      if (!dm.github.pr.title.match(/^\[\w+-\d+]/)) {
+        message(`PR title does not match expectation`)
+      }
+    }
+
+    dangerTesting(fn, { github: { pr: { title: 'hello world' } } })
+    expect(message).toHaveBeenCalledWith('PR title does not match expectation')
+    expect(message).toHaveBeenCalledTimes(1)
+    expect(fail).not.toHaveBeenCalled()
+    expect(markdown).not.toHaveBeenCalled()
+    expect(warn).not.toHaveBeenCalled()
+  })
+
+  it('should call `warning` function if criteria not met', () => {
+    const fn = () => {
+      if (!dm.github.pr.title.match(/^\[\w+-\d+]/)) {
+        warn(`PR title does not match expectation`)
+      }
+    }
+
+    dangerTesting(fn, { github: { pr: { title: 'hello world' } } })
+    expect(warn).toHaveBeenCalledWith('PR title does not match expectation')
+    expect(warn).toHaveBeenCalledTimes(1)
+    expect(fail).not.toHaveBeenCalled()
+    expect(markdown).not.toHaveBeenCalled()
+    expect(message).not.toHaveBeenCalled()
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import * as danger from 'danger'
 
-export let dm = danger
+export let dm = danger as any
 
 dm.fail = jest.fn()
+dm.markdown = jest.fn()
 dm.message = jest.fn()
+dm.warn = jest.fn()
 
 export const fail = dm.fail
+export const markdown = dm.markdown
 export const message = dm.message
 export const warn = dm.warn
 
-export const dangerTesting = (fn, obj) => {
+jest.mock('danger', () => jest.fn())
+
+export const dangerTesting = (fn: Function, obj) => {
   dm = {
-   ...dm,
-   ...obj
+    ...dm,
+    ...obj,
   }
   fn()
 }
